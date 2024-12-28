126162871b7c67af0475cc36b4974a3e2ee039e6
DhruvParashar673 <157940422+DhruvParashar673@users.noreply.github.com>
2024-12-28 10:32:53 +0000

    config done

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use  strict"//treat all  JS code as newer version
+//alert(3+3) we are using  node JS not browser so error will occur
+
+//alert("hello")
+//console.log(3+3)
+//console.log("dhruv")
+
+
+let name="hitesh" //string
+let age=18 //number
+let  isloggedIn=true //bool
+//number=> 2^53 range
+//bigint
+//string => ""
+// bool true/false
+//null => standalonevalue "KHALI H"
+//undefined=> defined hi nhi h value
+//symbol => unique ke liye use krte h
+
+//object 
+
+console.log(typeof null); //null ek object h!! 
+console.log(typeof undefined); //o/p undefined
+

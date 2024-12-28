88a673e6d463f5bbbb92cdf4fbbb76889ad670a1
DhruvParashar673 <157940422+DhruvParashar673@users.noreply.github.com>
2024-12-28 11:26:40 +0000

    connfig done

---
+++ basics/datatypeConversion.js
@@ -0,0 +1,29 @@
+/*let score="33ab"
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber)*/
+
+/*let score=null
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber)*/
+
+///"33" =>33
+//"33abc"=>NaN but type=NUMber
+//true =>1 false=>0
+let isloggedIn="hll"
+let boolean=Boolean(isloggedIn)
+console.log(boolean);
+
+//1=>true 0=>false
+//"" =>false  "hrr"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+ 

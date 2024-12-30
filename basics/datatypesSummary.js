e5ab88f2349df41abc030cc4bc55f18a5a7a3f7e
DhruvParashar673 <157940422+DhruvParashar673@users.noreply.github.com>
2024-12-30 08:44:47 +0000

    done

---
+++ basics/datatypesSummary.js
@@ -0,0 +1,25 @@
+ // Primitive Data types
+
+
+ //7  types-> string,Number,boolean,null,undefined,symbol,bigInt
+
+ ///Reference types(NON primitive)
+//arrays,objects,function
+
+///JS is Dynamicallly typed
+
+const bigNumber=3525544888888777n
+
+const heroes=["shaktiman",'nagraj','doga']
+let Myobj={
+    name:"dhruv",
+    age:22,
+
+}
+//{...} => object
+const myfun=function(){
+  console.log("hello  world");
+}
+ 
+console.log(typeof myfun)
+  

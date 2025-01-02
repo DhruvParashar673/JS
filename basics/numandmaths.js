3f3a5495138fe700898a04c2ebde69bfc70858a7
DhruvParashar673 <157940422+DhruvParashar673@users.noreply.github.com>
2025-01-02 23:34:12 +0000

    config

---
+++ basics/numandmaths.js
@@ -0,0 +1,36 @@
+const balance= new Number(400.5556)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const othernumber=23.8966
+//console.log(othernumber.toPrecision(3));
+
+const hundreds=10000000
+//console.log(hundreds.toLocaleString());
+//console.log(hundreds.toLocaleString('en-IN'));  //indiian representation
+
+///MATHS
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2)); //top value
+console.log(Math.floor(4.3)); //lowest value
+
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,36));
+
+
+console.log(Math.random()); //always give value between 0 to 1
+
+console.log((Math.random()*10)+1); //shift kr di !!
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
